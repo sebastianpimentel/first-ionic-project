cf380f63cf29e913470a274182da21a3afa8b4bf
sebastian <sebaspimen011@gmail.com>
2022-01-18 16:14:20 -0500

    first project ionic

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit{
 
   constructor() {}
 
+  ngOnInit() {
+    let num=20;
+    //debugge;//temporal
+    num=40;
+    num=num+25;
+    num=0;
+      console.log(num);
+      
+  }
 }
